d31f2a9412c20e152ee1e9be24a89ee97ec34667
Nil <n.bacardit@iskra.cat>
2019-09-04 18:21:39 +0200

    dev deploy superset

--- contrib/docker/superset_config.py
+++ contrib/docker/superset_config.py
@@ -1,19 +1,3 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 import os
 import jwt
 import requests
@@ -26,6 +10,8 @@ from flask_appbuilder.security.views import AuthRemoteUserView
 from flask_appbuilder.security.views import expose
 from flask_login import login_user
 
+
+# Create CM kubectl create configmap -n dev superset-configmap-data --from-file=../../contrib/docker/superset_config.py
 
 def get_env_variable(var_name, default=None):
     """Get the environment variable or raise exception."""
@@ -54,10 +40,11 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://%s:%s@%s:%s/%s' % (POSTGRES_USER,
                                                            POSTGRES_DB)
 REDIS_HOST = get_env_variable('REDIS_HOST')
 REDIS_PORT = get_env_variable('REDIS_PORT')
+REDIS_PASSWORD = get_env_variable('REDIS_PASSWORD')
 
 
 class CeleryConfig(object):
-    BROKER_URL = 'redis://%s:%s/0' % (REDIS_HOST, REDIS_PORT)
+    BROKER_URL = 'redis://%s@%s:%s/0' % (REDIS_PASSWORD, REDIS_HOST, REDIS_PORT)
     CELERY_IMPORTS = ('superset.sql_lab', )
     CELERY_RESULT_BACKEND = 'redis://%s:%s/1' % (REDIS_HOST, REDIS_PORT)
     CELERY_ANNOTATIONS = {'tasks.add': {'rate_limit': '10/s'}}
